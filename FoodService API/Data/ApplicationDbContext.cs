3db2265296d6e44d986049a2aef19e87cf7ce37f
Tshindimpho <mudzu13@gmail.com>
2024-09-16 08:45:29 +0200

    Create  Seed Menu Items

--- FoodService API/Data/ApplicationDbContext.cs
+++ FoodService API/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using FoodService_API.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
 
 namespace FoodService_API.Data
 {
@@ -11,6 +12,105 @@ namespace FoodService_API.Data
         }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        public DbSet<MenuItem> MenuItems { get; set; } 
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modlelbuilder)
+        {
+            base.OnModelCreating(modlelbuilder);
+            modlelbuilder.Entity<MenuItem>().HasData(
+            new MenuItem
+            {
+
+                    Id = 1,
+                    Name = "Spring Roll",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/spring roll.jpg",
+                    Price = 7.99,
+                    Category = "Appetizer",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 2,
+                    Name = "Idli",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/idli.jpg",
+                    Price = 8.99,
+                    Category = "Appetizer",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 3,
+                    Name = "Panu Puri",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/pani puri.jpg",
+                    Price = 8.99,
+                    Category = "Appetizer",
+                    SpecialTag = "Best Seller"
+                }, new MenuItem
+                {
+                    Id = 4,
+                    Name = "Hakka Noodles",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/hakka noodles.jpg",
+                    Price = 10.99,
+                    Category = "Entrée",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 5,
+                    Name = "Malai Kofta",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/malai kofta.jpg",
+                    Price = 12.99,
+                    Category = "Entrée",
+                    SpecialTag = "Top Rated"
+                }, new MenuItem
+                {
+                    Id = 6,
+                    Name = "Paneer Pizza",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/paneer pizza.jpg",
+                    Price = 11.99,
+                    Category = "Entrée",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 7,
+                    Name = "Paneer Tikka",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/paneer tikka.jpg",
+                    Price = 13.99,
+                    Category = "Entrée",
+                    SpecialTag = "Chef's Special"
+                }, new MenuItem
+                {
+                    Id = 8,
+                    Name = "Carrot Love",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/carrot love.jpg",
+                    Price = 4.99,
+                    Category = "Dessert",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 9,
+                    Name = "Rasmalai",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/rasmalai.jpg",
+                    Price = 4.99,
+                    Category = "Dessert",
+                    SpecialTag = "Chef's Special"
+                }, new MenuItem
+                {
+                    Id = 10,
+                    Name = "Sweet Rolls",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://foodserviceimages.blob.core.windows.net/foodservice/sweet rolls.jpg",
+                    Price = 3.99,
+                    Category = "Dessert",
+                    SpecialTag = "Top Rated"
+                
+            });
+        }
     }
 }
---
+++ FoodService API/Migrations/20240916064311_InitialSeedMenuItem.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodService_API.Migrations
+{
+    public partial class InitialSeedMenuItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MenuItems",
+                columns: new[] { "Id", "Category", "Description", "Image", "Name", "Price", "SpecialTag" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/spring roll.jpg", "Spring Roll", 7.9900000000000002, "" },
+                    { 2, "Appetizer", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/idli.jpg", "Idli", 8.9900000000000002, "" },
+                    { 3, "Appetizer", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/pani puri.jpg", "Panu Puri", 8.9900000000000002, "Best Seller" },
+                    { 4, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/hakka noodles.jpg", "Hakka Noodles", 10.99, "" },
+                    { 5, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/malai kofta.jpg", "Malai Kofta", 12.99, "Top Rated" },
+                    { 6, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/paneer pizza.jpg", "Paneer Pizza", 11.99, "" },
+                    { 7, "Entrée", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/paneer tikka.jpg", "Paneer Tikka", 13.99, "Chef's Special" },
+                    { 8, "Dessert", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/carrot love.jpg", "Carrot Love", 4.9900000000000002, "" },
+                    { 9, "Dessert", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/rasmalai.jpg", "Rasmalai", 4.9900000000000002, "Chef's Special" },
+                    { 10, "Dessert", "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://foodserviceimages.blob.core.windows.net/foodservice/sweet rolls.jpg", "Sweet Rolls", 3.9900000000000002, "Top Rated" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
