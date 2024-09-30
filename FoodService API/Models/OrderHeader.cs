c1c52a06067c94e4bedf9f5d4312ce0023741979
Tshindimpho <mudzu13@gmail.com>
2024-09-30 21:03:51 +0200

    Add OrderDetails and OrderHeader Models

---
+++ FoodService API/Models/OrderHeader.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodService_API.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int OrderHeaderId { get; set; }
+        [Required]
+        public string PickUpName { get; set; }
+        [Required]
+        public string PickUpPhoneNumber { get; set; }
+        [Required]
+        public string PickUpEmail { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser User { get; set; } 
+        public double OrderTotal { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntentID { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+    }
+}
