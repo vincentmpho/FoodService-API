07e0431601c1bfad89a59310e448194a1d5bf444
Tshindimpho <mudzu13@gmail.com>
2024-09-30 21:14:44 +0200

    Implement DTO

---
+++ FoodService API/Models/DTOs/OrderDetailsCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodService_API.Models.DTOs
+{
+    public class OrderDetailsCreateDto
+    {
+      
+        [Required]
+        public int MenuItemId { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ FoodService API/Models/DTOs/OrderHeaderUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodService_API.Models.DTOs
+{
+    public class OrderHeaderUpdateDto
+    {
+       
+        public int OrderHeaderId { get; set; }
+        public string PickUpName { get; set; }
+        public string PickUpPhoneNumber { get; set; }
+        public string PickUpEmail { get; set; }
+       
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntentID { get; set; }
+        public string Status { get; set; }
+    }
+}
