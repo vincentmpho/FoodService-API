0f74bf7ade260ddbc4e369b0bfd0dcaa73068412
Tshindimpho <mudzu13@gmail.com>
2024-10-02 15:46:04 +0200

    Implement Order Controller

--- FoodService API/Utility/SD.cs
+++ FoodService API/Utility/SD.cs
@@ -5,5 +5,12 @@
         public const string SD_Storage_Container = "foodservice";
         public const string Role_Admin = "admin";
         public const string Role_Customer = "customer";
+
+        public const string status_pending = "Pending";
+        public const string status_confirmed = "Confirmed";
+        public const string status_beingCooked = "Being Cooked";
+        public const string status_readyForPickUp = "Ready For Pickup";
+        public const string status_Completed = "Completed";
+        public const string status_Cancelled = "Cancelled";
     }
 }
